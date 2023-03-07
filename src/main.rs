309d44c69c147810163110d7d7cfa195d1d5feca
Egg55 <emilianomilanog@gmail.com>
2023-03-07 15:19:21 -0700

    formating and tests

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use axum::{routing::get, Router, Server};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root_get));
-    let server = Server::bind(&"127.0.0.1:7474".parse().unwrap()).serve(router.into_make_service());
-    let addr = server.local_addr();    
+    let app = Router::new()
+        .route("/", get(root_get));
+
+    let server = Server::bind(&"127.0.0.1:7474".parse().unwrap()).serve(app.into_make_service());
+    
+    let addr = server.local_addr();
     println!("Listening On {addr}");
+    
     server.await.unwrap();
 }
 
