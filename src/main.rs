b7d0e97ffcb9f49c035f12380ae78c709ef296a9
Egg55 <emilianomilanog@gmail.com>
2023-03-07 15:52:10 -0700

    Got cpu usage to disp, imported sysinfo

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
-use axum::{routing::get, Router, Server};
+use axum::{routing::get, Router, Server, extract::State};
+use sysinfo::{CpuExt, System, SystemExt};
+use std::sync::{Arc, Mutex};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root_get));
+        .route("/", get(root_get))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new()) )
+        });
 
     let server = Server::bind(&"127.0.0.1:7474".parse().unwrap()).serve(app.into_make_service());
     
     let addr = server.local_addr();
-    println!("Listening On {addr}");
-    
+    println!("Listening On {addr}");System::new();
+
     server.await.unwrap();
 }
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
 
-async fn root_get() -> &'static str {
-    "Hi from Axum"
+async fn root_get(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+
+    let mut s = String::new();
+
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu();
+
+
+    for (index, cpu) in sys.cpus().iter().enumerate() {
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {index} {usage}%").unwrap();
+    }
+    
+    s
 }
