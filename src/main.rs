9e608af04166e84029f2e05c37df7c416c13b261
Egg55 <emilianomilanog@gmail.com>
2023-03-07 15:04:48 -0700

    Setup files with axum and tokio

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{routing::get, Router, Server};
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(root_get));
+    let server = Server::bind(&"127.0.0.1:7474".parse().unwrap()).serve(router.into_make_service());
+    let addr = server.local_addr();    
+    println!("Listening On {addr}");
+    server.await.unwrap();
+}
+
+async fn root_get() -> &'static str {
+    "Hi from Axum"
+}
