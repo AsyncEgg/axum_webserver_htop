a397c429d2f17e887a1abde16dc5df788f80b496
Egg55 <emilianomilanog@gmail.com>
2023-03-09 02:13:00 -0700

    Disp cpu preact pretty

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,26 @@
 import { h, Component, render } from 'https://esm.sh/preact';
-    
+import htm from 'https://esm.sh/htm';
+
+const html = htm.bind(h);
+
+function App(props) {
+    return html`
+    <div>
+    ${props.cpus.map((cpu) => {
+        return html`
+            <div>${cpu.toFixed(2)}% usage</div>
+        `;
+    })}
+    </div>
+    `;
+}
+
 setInterval(async () => {
     let responce = await fetch('/api/cpus');
-    if ( responce.status !== 200 ){
+    if (responce.status !== 200) {
         throw new Error(`HTTP error! status: ${responce.status}`);
     }
 
     let json = await responce.json();
-    //document.body.textContent = JSON.stringify(json, null, 2);
-
-    const app = h('pre', null, JSON.stringify(json, null, 2));
-
-    render(app, document.body)
+    render(html`<${App} cpus=${json}></${App}>`, document.body)
 }, 1000);
